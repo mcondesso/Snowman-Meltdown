5dfb56e14e4f50024a46d6baf22c2d186633caaf
mcondesso <mcondesso@mail.com>
2026-04-15 14:04:36 +0200

    Added replay logic

--- snowman.py
+++ snowman.py
@@ -3,6 +3,16 @@ from game_logic import play_game
 
 def main():
     play_game()
+    while True:
+        answer = input("Would you like to play again? (yes/no)").lower()
+        if answer not in {"yes", "no"}:
+            print("Please enter 'yes' or 'no'.\n")
+            continue
+        if answer == "yes":
+            play_game()
+        else:
+            print("Thank you for playing!")
+            break
 
 
 if __name__ == "__main__":
