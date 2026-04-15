731213f8e195afddae94c9e1955db9da95148bec
mcondesso <mcondesso@mail.com>
2026-04-15 13:36:25 +0200

    Added main loop, mistake and game over logic

--- snowman.py
+++ snowman.py
@@ -3,19 +3,76 @@ import random
 # List of secret words
 WORDS = ["python", "git", "github", "snowman", "meltdown"]
 
+# Snowman ASCII Art stages
+STAGES = [
+     # Stage 0: Full snowman
+     """
+      ___  
+     /___\\ 
+     (o o) 
+     ( : ) 
+     ( : ) 
+     """,
+     # Stage 1: Bottom part starts melting
+     """
+      ___  
+     /___\\ 
+     (o o) 
+     ( : ) 
+     """,
+     # Stage 2: Only the head remains
+     """
+      ___  
+     /___\\ 
+     (o o) 
+     """,
+     # Stage 3: Snowman completely melted
+     """
+      ___  
+     /___\\ 
+     """
+ ]
+
 def get_random_word():
     """Selects a random word from the list."""
     return WORDS[random.randint(0, len(WORDS) - 1)]
 
+
+def display_game_state(mistakes, secret_word, guessed_letters):
+    print(STAGES[mistakes])
+    text = "WORD: "
+    if mistakes < len(STAGES) - 1:
+        for l in secret_word:
+            if l in guessed_letters:
+                text += l
+            else:
+                text += "_"
+            text += " "
+        print(text.rstrip(), "\n")
+
+
+def display_game_over(secret_word):
+    print("Game Over! The word was: ", secret_word)
+    print(STAGES[-1])
+
+
 def play_game():
     secret_word = get_random_word()
     print("Welcome to Snowman Meltdown!")
-    print("Secret word selected: " + secret_word)  # for testing, later remove this line
 
-    # TODO: Build your game loop here.
-    # For now, simply prompt the user once:
-    guess = input("Guess a letter: ").lower()
-    print("You guessed:", guess)
+    mistakes = 0
+    guessed_letters = []
+    while mistakes < len(STAGES) - 1:
+        display_game_state(mistakes, secret_word, guessed_letters)
+
+        guess = input("Guess a letter: ").lower()
+        guessed_letters.append(guess)
+        if guess not in secret_word:
+            mistakes += 1
+
+    if mistakes == len(STAGES) - 1:
+        display_game_over(secret_word)
+
     
 if __name__ == "__main__":
     play_game()
